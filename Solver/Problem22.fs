ab589908ea81799ddc9c808a14b92be0b2e98a5a
Binwei <summersnowe@gmail.com>
2015-12-07 17:58:14 +0800

    Complete problem 22

--- Solver/Problem22.fs
+++ Solver/Problem22.fs
@@ -4,5 +4,21 @@ open System
 open System.IO
 
 let readAllNames(path) =
-    File.ReadAllText(path).Split([|',';';'|], StringSplitOptions.RemoveEmptyEntries)
+    File.ReadAllText(path).Split([|',';'"'|], StringSplitOptions.RemoveEmptyEntries)
     |> Seq.sort
+
+let computeName(name:string) =
+    let bytes = System.Text.Encoding.ASCII.GetBytes(name)
+    Seq.sumBy (fun b -> (int)b - 64) bytes
+
+let getNameScore(names:seq<string>, idx) = 
+    let name = Seq.nth idx names
+    (idx+1) * computeName(name)
+
+let computeAll(path) =
+    let names = readAllNames path
+    let mutable sum = 0
+    for i in [0..Seq.length(names)-1] do
+        sum <- sum + getNameScore(names, i)
+    sum
+
--- SolverTest/Problem22.Test.fs
+++ SolverTest/Problem22.Test.fs
@@ -6,7 +6,16 @@ open System
 type Problem22Tests() =
 
     [<Test>]
-    member this.readAllNamesTest() =
+    member this.getNameScoreTest() =
         let names = Problem22.readAllNames("Problem22_names.txt")
         let name = Seq.nth 937 names
         Assert.AreEqual("COLIN", name)
+        Assert.AreEqual(49714, Problem22.getNameScore(names, 937))
+
+    [<Test>]
+    member this.computeNameTest() =
+        Assert.AreEqual(53, Problem22.computeName("COLIN"))
+
+    [<Test>]
+    member this.computeAllTest() =
+        Assert.AreEqual(0, Problem22.computeAll("Problem22_names.txt"))
