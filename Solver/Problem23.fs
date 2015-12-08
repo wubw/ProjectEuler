6118aafe96d6d604467cf6fd62e9d9ca5df9c2cf
Binwei <summersnowe@gmail.com>
2015-12-08 14:23:21 +0800

    Problem23

---
+++ Solver/Problem23.fs
@@ -0,0 +1,37 @@
+﻿module ProjectEuler.Solver.Problem23
+
+open System
+open System.Collections.Generic
+open ProjectEuler.Solver
+
+let isPerfectNum(num:int) =
+    Problem21.getSumOfProperDivisors(num) = num
+
+let isDeficientNum(num:int) =
+    Problem21.getSumOfProperDivisors(num) < num
+
+let isAbundantNum(num:int) =
+    Problem21.getSumOfProperDivisors(num) > num
+
+let rec getAllAbundantNum(num:int) = 
+    if num <= 28123 then 
+        if isAbundantNum(num) then 
+            num::getAllAbundantNum(num+1)
+        else getAllAbundantNum(num+1)
+    else []
+
+let computeNums() =
+    let allAbundantNums = getAllAbundantNum(1)
+    let ret = HashSet<int>()
+    for i in allAbundantNums do
+        for j in allAbundantNums do
+            ret.Add(i+j) |> ignore
+    ret
+
+let getFinalResults() = 
+    let mutable ret = 0
+    let nums = computeNums()
+    for i in [1..28123] do
+        if not (nums.Contains(i)) then
+            ret <- ret+i
+    ret
---
+++ SolverTest/Problem23.Test.fs
@@ -0,0 +1,27 @@
+﻿module ProjectEuler.Solver.Problem23
+open NUnit.Framework
+open System
+
+[<TestFixture>]
+type Problem23Tests() =
+
+    [<Test>]
+    member this.isPerfectNumTest() =
+        Assert.IsTrue(Problem23.isPerfectNum(28))
+        Assert.IsTrue(Problem23.isAbundantNum(12))
+
+    [<Test>]
+    member this.getAllAbundantNumTest() = 
+        let allAbundantNums = Problem23.getAllAbundantNum(1)
+        Assert.AreEqual(allAbundantNums.Head, 12)
+        Assert.AreEqual(Seq.length(allAbundantNums), 6965)
+
+    [<Test>]
+    member this.computeNumsTest() = 
+        let nums = Problem23.computeNums()
+        Assert.AreEqual(24, Seq.min(nums))
+
+    [<Test>]
+    member this.getFinalResultsTest() = 
+        let result = Problem23.getFinalResults()
+        Assert.AreEqual(0, result)
