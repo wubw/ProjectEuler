cc50d7ad2d49d7fcbfb12ba4d950249ae646eb75
Binwei <summersnowe@gmail.com>
2015-12-29 16:51:32 +0800

    Submit Python files

---
+++ PythonSolver/Problem25.py
@@ -0,0 +1,14 @@
+﻿idx = 3
+num1 = 1;
+num2 = 1;
+
+# Fibonacci 
+while True:
+    cur = num1 + num2
+    strCnt = len(str(cur))
+    val = (idx, cur, strCnt)
+    idx = idx + 1
+    num1, num2 = num2, cur
+    if strCnt == 1000:
+        print(val)
+        break
